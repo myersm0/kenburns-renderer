0fdadfbb8deed8b487abe597091a90297964c7de
Michael Myers <michael.myers@wustl.edu>
2026-03-15 18:08:43 -0500

    successful

--- slideshow.h
+++ slideshow.h
@@ -294,15 +294,18 @@ public:
 
 	bool swap() {
 		if (phase == SlideshowPhase::Transitioning && next_pyramid) {
-			if (current_pyramid) delete current_pyramid;
-			current_pyramid = next_pyramid;
-			next_pyramid = nullptr;
-			current_keyframe = next_keyframe;
-			current_points = next_points;
-			current_frame = 0;
-			fade_complete_flag = false;
-			phase = SlideshowPhase::Holding;
-			return true;
+			 if (current_pyramid) delete current_pyramid;
+			 current_pyramid = next_pyramid;
+			 next_pyramid = nullptr;
+			 current_keyframe = next_keyframe;
+			 current_points = next_points;
+			 // maintain animation continuity
+			 double fade_progress = (double)(current_frame - transition_start_frame) / fade_frames;
+			 fade_progress = std::min(fade_progress, 1.0);
+			 current_frame = (int)(fade_progress * fade_frames / (double)total_frames * total_frames);
+			 fade_complete_flag = false;
+			 phase = SlideshowPhase::Holding;
+			 return true;
 		}
 		if (phase == SlideshowPhase::Holding && loader.ready()) {
 			ImagePyramid* incoming = loader.collect();
@@ -370,8 +373,7 @@ public:
 			}
 		}
 
-		if (!fade_complete_flag)
-			current_frame++;
+		current_frame++;
 
 		return params;
 	}
